7821e0cc3d7ea5785ab1e8a6b7f05e25e3324d1f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-24 00:52:05 +0700

    Add fetchProducts to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
-import { Input, Layout } from 'antd';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Input, Layout, Row, Col, Card } from 'antd';
 import './styles/App.css';
 
+const { Meta } = Card;
 const { Header, Content } = Layout;
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const initiate = async() => {
+    const fetchProducts = await axios.get('https://react-workshop-v1.herokuapp.com/products');
+    setProducts(fetchProducts.data);
+  };
+
+  useEffect(() => {
+    initiate();
+  }, []);
+
+  const mapProducts = products.map((product) => ({ ...product, images: product.images.split('|') }));
+
+  const renderedProducts = mapProducts.map((mapProduct) =>
+    <Col
+      key={mapProduct.id}
+      span={8}
+      style={{ marginTop: '12px', marginBottom: '12px' }}>
+      <Card
+        hoverable
+        cover={<img alt={mapProduct.postName} src={mapProduct.images[0]} />}
+      >
+        <Meta
+          title={mapProduct.postTitle}
+          description={mapProduct.postContent} />
+      </Card>
+    </Col>
+  );
+
   return (
     <Layout>
       <Header style={{ color: 'white', fontSize: '24px', textAlign: 'center' }}>
         React Workshop
       </Header>
       <Content style={{ maxWidth: '1024px', width: '100%', margin: 'auto', padding: '32px' }}>
-        <Input placeholder="Search Products"></Input>
+        <Input placeholder="Search Products" style={{ borderRadius: '16px' }} />
+        {
+          mapProducts
+            ? <Row style={{ marginTop: '12px' }} gutter={{ xs: 8, sm: 16, md: 24 }}>
+              {renderedProducts}
+            </Row>
+            : undefined
+        }
       </Content>
     </Layout>
   );
