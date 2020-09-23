2ebfec68b95022a8f84fd7f953c1ea731ab59f6b
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-24 00:06:26 +0700

    Add antd dependencies and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Input, Layout } from 'antd';
+import './styles/App.css';
+
+const { Header, Content } = Layout;
 
 const App = () => {
   return (
-    <div>
-      Hello App
-    </div>
+    <Layout>
+      <Header style={{ color: 'white', fontSize: '24px', textAlign: 'center' }}>
+        React Workshop
+      </Header>
+      <Content style={{ maxWidth: '1024px', width: '100%', margin: 'auto', padding: '32px' }}>
+        <Input placeholder="Search Products"></Input>
+      </Content>
+    </Layout>
   );
 };
 
