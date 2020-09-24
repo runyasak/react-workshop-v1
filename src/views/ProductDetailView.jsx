ef6a2ce42b270d84b4f6b13a179643bce430ee93
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-25 01:06:10 +0700

    Add fetchProductById to ProductDetailView

--- src/views/ProductDetailView.jsx
+++ src/views/ProductDetailView.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Row, Col, Image } from 'antd';
 
 const ProductDetailView = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  const initiate = async() => {
+    const fetchProductById = await axios.get(`https://react-workshop-v1.herokuapp.com/products/${id}`);
+    setProduct(fetchProductById.data);
+  };
+
+  useEffect(() => {
+    initiate();
+  }, []);
+
+  const mapProduct = {
+    ...product,
+    images: product ? product.images.split('|')[0] : ''
+  };
+
   return (
     <div className="product-detial-view">
-      Product Detail
+      <Row style={{ marginTop: '12px' }} gutter={{ xs: 8, sm: 16, md: 24 }}>
+        <Col span={12}>
+          <Image src={mapProduct.images}></Image>
+        </Col>
+        <Col span={12}>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
+            <span style={{ fontSize: '16px', fontWeight: '500' }}>{mapProduct.postTitle}</span>
+            <span style={{ fontSize: '24px' }}>${mapProduct.regularPrice || 0}</span>
+          </div>
+          <div>
+            {mapProduct.postContent}
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
