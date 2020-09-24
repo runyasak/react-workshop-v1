8718a274d13c3c5eb32603916c1333cd9c78bdd2
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-25 00:20:36 +0700

    Add HomeView

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
-import axios from 'axios';
-import { Input, Layout, Row, Col, Card } from 'antd';
+import { Layout } from 'antd';
+import HomeView from './views/HomeView';
 import './styles/main.css';
 
-const { Meta } = Card;
 const { Header, Content } = Layout;
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-
-  const initiate = async() => {
-    const fetchProducts = await axios.get('https://react-workshop-v1.herokuapp.com/products');
-    setProducts(fetchProducts.data);
-  };
-
-  useEffect(() => {
-    initiate();
-  }, []);
-
-  const mapProducts = products.map((product) => ({ ...product, images: product.images.split('|') }));
-
-  const renderedProducts = mapProducts.map((mapProduct) =>
-    <Col
-      key={mapProduct.id}
-      span={24}
-      md={8}
-      style={{ marginTop: '12px', marginBottom: '12px' }}>
-      <Card
-        hoverable
-        cover={<img alt={mapProduct.postName} src={mapProduct.images[0]} />}
-      >
-        <Meta
-          title={
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
-              <span>{mapProduct.postTitle}</span>
-              <span style={{ fontSize: '24px' }}>${mapProduct.regularPrice || 0}</span>
-            </div>}
-          description={
-            <div className="line-clamp-2">{mapProduct.postContent}</div>
-          }
-        />
-      </Card>
-    </Col>
-  );
-
   return (
     <Router>
       <Layout>
@@ -61,14 +23,7 @@ const App = () => {
               Product detail
             </Route>
             <Route path="/">
-              <Input placeholder="Search Products" style={{ fontSize: '24px' }} />
-              {
-                mapProducts
-                  ? <Row style={{ marginTop: '12px' }} gutter={{ xs: 8, sm: 16, md: 24 }}>
-                    {renderedProducts}
-                  </Row>
-                  : undefined
-              }
+              <HomeView />
             </Route>
           </Switch>
         </Content>
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Input, Row, Col, Card } from 'antd';
+
+const { Meta } = Card;
+
+const HomeView = () => {
+  const [products, setProducts] = useState([]);
+
+  const initiate = async() => {
+    const fetchProducts = await axios.get('https://react-workshop-v1.herokuapp.com/products');
+    setProducts(fetchProducts.data);
+  };
+
+  useEffect(() => {
+    initiate();
+  }, []);
+
+  const mapProducts = products.map((product) => ({ ...product, images: product.images.split('|') }));
+
+  const renderedProducts = mapProducts.map((mapProduct) =>
+    <Col
+      key={mapProduct.id}
+      span={24}
+      md={8}
+      style={{ marginTop: '12px', marginBottom: '12px' }}>
+      <Card
+        hoverable
+        cover={<img alt={mapProduct.postName} src={mapProduct.images[0]} />}
+      >
+        <Meta
+          title={
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
+              <span>{mapProduct.postTitle}</span>
+              <span style={{ fontSize: '24px' }}>${mapProduct.regularPrice || 0}</span>
+            </div>
+          }
+          description={
+            <div className="line-clamp-2">{mapProduct.postContent}</div>
+          }
+        />
+      </Card>
+    </Col>
+  );
+
+  return (
+    <div className="home-view">
+      <Input placeholder="Search Products" style={{ fontSize: '24px' }} />
+      {
+        mapProducts
+          ? <Row style={{ marginTop: '12px' }} gutter={{ xs: 8, sm: 16, md: 24 }}>
+            {renderedProducts}
+          </Row>
+          : undefined
+      }
+    </div>
+  );
+};
+
+export default HomeView;
